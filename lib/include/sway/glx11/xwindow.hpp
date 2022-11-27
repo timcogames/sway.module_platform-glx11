5ad0c99db1c2ba44bb88f48e2a066566fdb0cab7
apriori85 <victor-timoshin@hotmail.com>
2022-11-27 09:16:36 +0300

    Refactoring

--- lib/include/sway/glx11/xwindow.hpp
+++ lib/include/sway/glx11/xwindow.hpp
@@ -17,165 +17,138 @@ typedef std::map<s32_t, EventCallbackFunc_t> EventCallbackFuncMap_t;
 
 class XWindow : public WindowEventListener {
 public:
-  /*!
-   * \brief
-   *    Конструктор класса.
+  /**
+   * @brief Конструктор класса.
+   * Выполняет инициализацию нового экземпляра класса.
    *
-   *    Выполняет инициализацию нового экземпляра класса.
+   * @param[in] connection Экранное соедининение с сервером.
    *
-   * \param[in] connection
-   *    Экранное соедининение с сервером.
    */
   XWindow(XScreenConnectionRef_t connection);
 
-  /*!
-   * \brief
-   *    Деструктор класса.
+  /**
+   * @brief Деструктор класса.
+   * Уничтожает главное окно приложения.
    *
-   *    Уничтожает главное окно приложения.
    */
   ~XWindow();
 
-  /*!
-   * \brief
-   *    Добавляет привязку функции к оконному событию.
+  /**
+   * @brief Добавляет привязку функции к оконному событию.
    *
-   * \param[in] type
-   *    Тип события.
+   * @param[in] type Тип события.
+   * @param[in] callback Функция вызываемая при возникновении события.
    *
-   * \param[in] callback
-   *    Функция вызываемая при возникновении события.
    */
   void addEventBinding(s32_t type, EventCallbackFunc_t callback);
 
-  /*!
-   * \brief
-   *    Создает главное окно приложения.
+  /**
+   * @brief Создает главное окно приложения.
    *
-   * \param[in] visualInfo
-   *    Информация о дисплее.
+   * @param[in] visualInfo Информация о дисплее.
+   * @param[in] windowInfo Начальные параметры окна.
    *
-   * \param[in] windowInfo
-   *    Начальные параметры окна.
+   * @exception std::runtime_error Неудачное создание окна.
    *
-   * \exception std::runtime_error Неудачное создание окна.
    */
   void createDummy(XVisualInfo *visualInfo, const WindowInitialInfo &windowInfo);
 
-  /*!
-   * \brief
-   *    Обрабатывает события.
+  /**
+   * @brief Обрабатывает события.
+   *
    */
   bool eventLoop(bool keepgoing);
 
-  /*!
-   * \brief
-   *    Устанавливает заголовок окна.
+  /**
+   * @brief Устанавливает заголовок окна.
    *
-   * \param[in] title
-   *    Заголовок окна.
+   * @param[in] title Заголовок окна.
+   *
    */
   void setTitle(lpcstr_t title);
 
-  /*!
-   * \brief
-   *    Устанавливает позицию окна.
+  /**
+   * @brief Устанавливает позицию окна.
    *
-   * \param[in] x
-   *    Координата позиции окна по оси X.
+   * @param[in] x Координата позиции окна по оси X.
+   * @param[in] y Координата позиции окна по оси Y.
    *
-   * \param[in] y
-   *    Координата позиции окна по оси Y.
    */
   void setPosition(s32_t x, s32_t y);
 
-  /*!
-   * \brief
-   *    Получает позицию окна.
+  /**
+   * @brief Получает позицию окна.
+   *
    */
   auto getPosition() const -> math::point2i_t;
 
-  /*!
-   * \brief
-   *    Устанавливает размер окна.
+  /**
+   * @brief Устанавливает размер окна.
    *
-   * \param[in] w
-   *    Ширина окна.
+   * @param[in] w Ширина окна.
+   * @param[in] h Высота окна.
    *
-   * \param[in] h
-   *    Высота окна.
    */
   void setSize(s32_t w, s32_t h);
 
-  /*!
-   * \brief
-   *    Получает размер окна.
+  /**
+   * @brief Получает размер окна.
+   *
    */
   auto getSize() const -> math::size2i_t;
 
-  /*!
-   * \brief
-   *    Устанавливает поведение при смене размера.
+  /**
+   * @brief Устанавливает поведение при смене размера.
    *
-   * \param[in] size
-   *    Размер окна.
+   * @param[in] size Размер окна.
+   * @param[in] resizable Имеется возможность изменения размера?
    *
-   * \param[in] resizable
-   *    Имеется возможность изменения размера?
    */
   void setSizeHints(const WindowSize &size, bool resizable);
 
-  /*!
-   * \brief
-   *    Показывает окно.
+  /**
+   * @brief Показывает окно.
    *
-   * \sa
-   *    hide()
-   *    visible()
+   * @sa hide(), visible()
+   *
    */
   void show();
 
-  /*!
-   * \brief
-   *    Скрывает окно.
+  /**
+   * @brief Скрывает окно.
    *
-   * \sa
-   *    show()
-   *    visible()
+   * @sa show(), visible()
+   *
    */
   void hide();
 
-  /*!
-   * \brief
-   *    Получает значение видимости окна.
+  /**
+   * @brief Получает значение видимости окна.
    *
-   * \sa
-   *    show()
-   *    hide()
+   * @sa show(), hide()
+   *
    */
   bool visible() const;
 
-  /*!
-   * \brief
-   *    Переключает в полноэкранный / оконный режим.
+  /**
+   * @brief Переключает в полноэкранный / оконный режим.
    *
-   * \param[in] fullscreen
-   *    Включить полноэкранный режим?
+   * @param[in] fullscreen Включить полноэкранный режим?
+   *
    */
   void setFullscreen(bool fullscreen);
 
-  /*!
-   * \brief
-   *    Устанавливает максимальный размер окна.
+  /**
+   * @brief Устанавливает максимальный размер окна.
    *
-   * \param[in] maximized
-   *    Растянуть окно до максимальных размеров?
+   * @param[in] maximized Растянуть окно до максимальных размеров?
+   *
    */
   void setMaximize(bool maximized);
 
-  /*!
-   * \brief
-   *    Получает идентификатор окна.
+  /**
+   * @brief Получает идентификатор окна.
+   *
    */
   auto getWindowHandle() -> Window;
 
@@ -184,33 +157,23 @@ private:
 
   void _initializeEventBindings();
 
-  /*!
-   * \brief
-   *    Устанавливает минимальный размер.
+  /**
+   * @brief Устанавливает минимальный размер.
    *
-   * \param[out] hints
-   *    Cтруктура рекомендаций.
+   * @param[out] hints Cтруктура рекомендаций.
+   * @param[in] size Размер окна.
+   * @param[in] resizable Имеется возможность изменения размера?
    *
-   * \param[in] size
-   *    Размер окна.
-   *
-   * \param[in] resizable
-   *    Имеется возможность изменения размера?
    */
   void _setMinSize(XSizeHints *hints, const WindowSize &size, bool resizable);
 
-  /*!
-   * \brief
-   *    Устанавливает максимальный размер.
+  /**
+   * @brief Устанавливает максимальный размер.
    *
-   * \param[out] hints
-   *    Cтруктура рекомендаций.
+   * @param[out] hints Cтруктура рекомендаций.
+   * @param[in] size Размер окна.
+   * @param[in] resizable Имеется возможность изменения размера?
    *
-   * \param[in] size
-   *    Размер окна.
-   *
-   * \param[in] resizable
-   *    Имеется возможность изменения размера?
    */
   void _setMaxSize(XSizeHints *hints, const WindowSize &size, bool resizable);
 
@@ -224,4 +187,4 @@ private:
 NAMESPACE_END(glx11)
 NAMESPACE_END(sway)
 
-#endif
+#endif  // SWAY_GLX11_XWINDOW_HPP
