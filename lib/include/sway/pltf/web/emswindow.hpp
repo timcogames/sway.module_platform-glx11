49c188cab856d7fc989df286ddf928f58880bb3b
apriori85 <victor-timoshin@hotmail.com>
2023-09-04 08:06:29 +0300

    Added setFullscreen

--- lib/include/sway/pltf/web/emswindow.hpp
+++ lib/include/sway/pltf/web/emswindow.hpp
@@ -2,23 +2,39 @@
 #define SWAY_PLTF_WEB_EMSWINDOW_HPP
 
 #include <sway/core.hpp>
-#include <sway/pltf/windoweventlistener.hpp>
+#include <sway/pltf/context.hpp>
 #include <sway/pltf/windowinitialinfo.hpp>
 #include <sway/pltf/windowmodes.hpp>
 
+#include <condition_variable>
+#include <functional>
 #include <memory>
+#include <mutex>
+#include <queue>
 #include <string>
 
 NAMESPACE_BEGIN(sway)
 NAMESPACE_BEGIN(pltf)
 
-class EMSContext;
+enum class WindowEventType {
+  SIZE_CHANGE,
+  RESOLUTION_CHANGE,
+  FULLSCREEN_CHANGE,
+  SCREEN_CHANGE,
+  FOCUS_CHANGE,
+  CLOSE,
+  SHOW,
+  HIDE,
+  MINIMIZE,
+  MAXIMIZE,
+  RESTORE
+};
 
 class EMSWindow {
 public:
   using CallbackFunc_t = void (*)(void *);
 
-  EMSWindow(std::shared_ptr<EMSContext> context);
+  EMSWindow(std::shared_ptr<Context> context);
 
   ~EMSWindow() = default;
 
@@ -37,8 +53,61 @@ public:
    */
   [[nodiscard]] auto getSize() const -> math::size2i_t;
 
+  /**
+   * @brief Переключает в полноэкранный / оконный режим.
+   *
+   * @param[in] fullscreen Включить полноэкранный режим?
+   */
+  void setFullscreen(bool fullscreen);
+
+  void handleResize() {
+    Event sizeChangeEvent{WindowEventType::SIZE_CHANGE};
+    sizeChangeEvent.size = math::size2i_t(0, 0);
+    sendEvent(sizeChangeEvent);
+  }
+  // void handleFullscreenChange(bool fullscreen);
+
+  class Event final {
+  public:
+    Event() = default;
+    explicit Event(WindowEventType initType) noexcept
+        : type{initType} {}
+
+    WindowEventType type;
+
+    math::size2i_t size;
+    union {
+      bool fullscreen = false;
+      std::uintptr_t displayId;
+      bool focus;
+    };
+  };
+
+  void sendEvent(const EMSWindow::Event &event) {
+    std::unique_lock lock{eventQueueMutex_};
+    eventQueue_.push(event);
+    lock.unlock();
+    eventQueueCondition_.notify_all();
+  }
+
+  std::queue<EMSWindow::Event> getEvents(bool waitForEvents) {
+    std::unique_lock lock{eventQueueMutex_};
+
+    if (waitForEvents) {
+      eventQueueCondition_.wait(lock, [this]() noexcept { return !eventQueue_.empty(); });
+    }
+
+    auto result = std::move(eventQueue_);
+    eventQueue_ = {};
+    return result;
+  }
+
+  std::queue<EMSWindow::Event> eventQueue_;
+  std::mutex eventQueueMutex_;
+  std::condition_variable eventQueueCondition_;
+
 private:
-  std::shared_ptr<EMSContext> context_;
+  std::shared_ptr<Context> context_;
 };
 
 NAMESPACE_END(pltf)
--- lib/src/web/emswindow.cpp
+++ lib/src/web/emswindow.cpp
@@ -7,7 +7,7 @@
 NAMESPACE_BEGIN(sway)
 NAMESPACE_BEGIN(pltf)
 
-EMSWindow::EMSWindow(std::shared_ptr<EMSContext> context)
+EMSWindow::EMSWindow(std::shared_ptr<Context> context)
     : context_(context) {}
 
 auto EMSWindow::eventLoop(CallbackFunc_t func, void *arg, [[maybe_unused]] bool keepgoing) -> bool {
@@ -15,14 +15,54 @@ auto EMSWindow::eventLoop(CallbackFunc_t func, void *arg, [[maybe_unused]] bool 
   return true;
 }
 
-void EMSWindow::setSize(s32_t w, s32_t h) { emscripten_set_canvas_element_size(context_->getTargetId().c_str(), w, h); }
+void EMSWindow::setSize(s32_t w, s32_t h) {
+  auto ctx = std::static_pointer_cast<EMSContext>(context_);
+
+  emscripten_set_canvas_element_size(ctx->getTargetId().c_str(), w, h);
+}
 
 auto EMSWindow::getSize() const -> math::size2i_t {
+  auto ctx = std::static_pointer_cast<EMSContext>(context_);
+
   auto elementWt = 0.0;
   auto elementHt = 0.0;
-  emscripten_get_element_css_size(context_->getTargetId().c_str(), &elementWt, &elementHt);
+  emscripten_get_element_css_size(ctx->getTargetId().c_str(), &elementWt, &elementHt);
 
   return {(s32_t)elementWt, (s32_t)elementHt};
+}
+
+EM_BOOL onCanvasResizeCallback(int type, const void *, void *userData) {
+  if (type == EMSCRIPTEN_EVENT_CANVASRESIZED) {
+    const auto nativeWindow = static_cast<EMSWindow *>(userData);
+    nativeWindow->handleResize();
+    return EM_TRUE;
+  }
+
+  return EM_FALSE;
+}
+
+void EMSWindow::setFullscreen(bool fullscreen) {
+  auto ctx = std::static_pointer_cast<EMSContext>(context_);
+  auto highDpi = true;
+
+  EmscriptenFullscreenChangeEvent status = {0};
+  EMSCRIPTEN_RESULT result = emscripten_get_fullscreen_status(&status);
+  if (result == EMSCRIPTEN_RESULT_SUCCESS) {
+    if (fullscreen) {
+      EmscriptenFullscreenStrategy fullscreenStrategy;
+      fullscreenStrategy.scaleMode = EMSCRIPTEN_FULLSCREEN_SCALE_STRETCH;
+      fullscreenStrategy.canvasResolutionScaleMode =
+          highDpi ? EMSCRIPTEN_FULLSCREEN_CANVAS_SCALE_HIDEF : EMSCRIPTEN_FULLSCREEN_CANVAS_SCALE_STDDEF;
+      fullscreenStrategy.filteringMode =
+          EMSCRIPTEN_FULLSCREEN_FILTERING_DEFAULT;  // EMSCRIPTEN_FULLSCREEN_FILTERING_NEAREST;
+      fullscreenStrategy.canvasResizedCallback = onCanvasResizeCallback;
+      fullscreenStrategy.canvasResizedCallbackUserData = this;
+
+      result = emscripten_request_fullscreen_strategy(ctx->getTargetId().c_str(), EM_TRUE, &fullscreenStrategy);
+    } else {
+      result = emscripten_exit_fullscreen();
+    }
+  }
 }
 
 NAMESPACE_END(pltf)
