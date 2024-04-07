647c57ee1b466edb5579f2100d60438ac7992ec3
apriori85 <victor-timoshin@hotmail.com>
2024-04-07 18:00:17 +0300

    Updated submodule

--- lib/cxx/include/sway/pltf/web/emswindow.hpp
+++ lib/cxx/include/sway/pltf/web/emswindow.hpp
@@ -21,9 +21,7 @@ struct SizeChangedEventData : public core::foundation::EventData {
   math::size2i_t size;
 
   // clang-format off
-  MTHD_OVERRIDE(auto serialize() const -> std::string) {  // clang-format on
-    return "";
-  }
+  MTHD_OVERRIDE(auto serialize() const -> std::string) { return ""; }  // clang-format on
 
   MTHD_OVERRIDE(void deserialize(const std::string &jdata)) {}
 };
